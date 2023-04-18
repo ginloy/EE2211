0c5a9aeadace6805f577dce8fafa642572322b06
ginloy <liyongleow@gmail.com>
2023-04-18 13:07:27 +0800

    Done q2

--- T8/q2.py
+++ T8/q2.py
@@ -2,7 +2,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 
-LR = 0.03
+LR = 1
 ITERS = int(2e6)
 
 df = pd.read_csv("government-expenditure-on-education2.csv")
@@ -14,29 +14,30 @@ max_expenditure = df.total_expenditure_on_education.max()
 
 df["normalized_year"] = df.year / max_year
 
-df["normalized_expenditure"] = (
-    df.total_expenditure_on_education / max_expenditure
-)
+df["normalized_expenditure"] = df.total_expenditure_on_education / max_expenditure
 
 x_train = np.array(df.normalized_year).reshape(-1, 1)
-y_train = np.array(df.normalized_year).reshape(-1, 1)
+x_train = np.hstack((np.ones_like(x_train), x_train))
+y_train = np.array(df.normalized_expenditure).reshape(-1, 1)
 
 test_years = np.arange(1981, 2024).reshape(-1, 1)
 x_test = test_years / max_year
+x_test = np.hstack((np.ones_like(x_test), x_test))
 
-weights = np.random.rand(1, 1)
-bias = np.zeros((1, 1))
+weights = np.zeros((2, 1))
 
 iter = np.arange(1, ITERS + 1)
 cost = []
-for _ in range(ITERS):
-    y_pred = np.exp(-(x_train @ weights + bias))
+for epoch in range(ITERS):
+    y_pred = np.exp(-(x_train @ weights))
     loss = np.mean((y_pred - y_train) ** 2)
     cost.append(loss)
-    loss_grad  = 2 * (y_pred - y_train) / len(y_train)
-    out_grad = -loss_grad
-    bias -= LR * np.sum(out_grad, axis=0, keepdims=True)
+    loss_grad = 2 * (y_pred - y_train) / y_pred.shape[0]
+    out_grad = -loss_grad * y_pred
     weights -= LR * x_train.T @ out_grad
+
+    if epoch % int(2e5) == 0:
+        print(f"Epoch {epoch}:\tloss = {loss}")
 
 cost = np.array(cost)
 plt.plot(iter, cost)
@@ -44,10 +45,11 @@ plt.xlabel("Iteration")
 plt.ylabel("Cost")
 plt.show()
 
-y_pred = np.exp(-(x_test @ weights + bias))
+y_pred = np.exp(-(x_test @ weights))
 pred_expenditure = y_pred * max_expenditure
 
 plt.plot(test_years, pred_expenditure)
+plt.scatter(df.year, df.total_expenditure_on_education)
 plt.xlabel("Year")
 plt.ylabel("Expenditure")
 plt.show()
